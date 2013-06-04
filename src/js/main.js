60a1877f71b48a64f2d55c7f6849668d2c0273ef
Frédéric van der Essen <fvdessen@gmail.com>
2013-06-04 02:45:14 +0200

    better looking example :)

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,52 @@
+$(function(){
+    function getPos(el){
+        for(var lx=0, ly=0;
+            el != null;
+            lx += el.offsetLeft, ly += el.offsetTop, el = el.offsetParent);
+        return {x: lx, y:ly};
+    }
+    window.getPos = getPos;
+    
+    window.images = $('.image');
 
+    var imageHeight = $(images[0]).outerHeight(true);
+    var topImagePos = getPos(images[0]);
+
+    function getImagePos(index){
+        return topImagePos + (index * imageHeight);
+    }
+
+    var playstate = 'paused';
+    
+    function animation(){
+        if(playstate == 'playing'){
+            scrollTo(scrollX,scrollY + imageHeight);
+            setTimeout(function(){
+                requestAnimationFrame(animation);
+            },130);
+        }
+    };
+    function play(){
+        playstate = 'playing';
+        animation();
+    }
+    function pause(){
+        playstate = 'paused';
+    }
+    function stop(){
+        playstate = 'paused';
+        requestAnimationFrame(function(){scrollTo(scrollX,0)});
+    }
+    window.play = play;
+    window.pause = pause;
+    window.stop = stop;
+
+    $('.js-play').click(function(){ 
+        if( playstate === 'paused'){ 
+            play();
+        }else{
+            pause();
+        }
+    });
+    $('.js-stop').click(stop);
+});
