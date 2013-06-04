8845e623ab9e8955066076b6cbd8d071eff7ba47
Frédéric van der Essen <fvdessen@gmail.com>
2013-06-05 01:24:38 +0200

    loop and frame skip

--- src/js/main.js
+++ src/js/main.js
@@ -17,10 +17,29 @@ $(function(){
     }
 
     var playstate = 'paused';
+
+    function nextFrame(){
+        var screenHeight = $(window).height();
+        var pageHeight = $(document).height();
+        if(scrollY + screenHeight + imageHeight > pageHeight){
+            scrollTo(scrollX,0);
+        }else{
+            scrollTo(scrollX,scrollY + imageHeight);
+        }
+    }
+    function prevFrame(){
+        var screenHeight = $(window).height();
+        var pageHeight = $(document).height();
+        if(scrollY === 0){
+            scrollTo(scrollX,pageHeight - imageHeight);
+        }else{
+            scrollTo(scrollX,scrollY - imageHeight);
+        }
+    }
     
     function animation(){
         if(playstate == 'playing'){
-            scrollTo(scrollX,scrollY + imageHeight);
+            nextFrame();
             setTimeout(function(){
                 requestAnimationFrame(animation);
             },130);
@@ -49,4 +68,16 @@ $(function(){
         }
     });
     $('.js-stop').click(stop);
+    $('.image').mousedown(function(event){
+        if(playstate === 'paused'){
+            if(event.which === 1){
+                nextFrame();
+            }else{
+                prevFrame();
+                event.preventDefault();
+            }
+        }else{
+            pause();
+        }
+    });
 });
